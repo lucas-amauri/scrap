5eda043d5b7b519d9e5d5ff0b8d022b1df55a19f
Lucas Amauri Oliveira <lucasamauri.ls@gmail.com>
2017-11-14 13:38:50 -0200

    Added follow locatio option

--- Scrap.php
+++ Scrap.php
@@ -3,7 +3,7 @@
 class Scrap {
 	private $SSL_VERSION = 1;
 	private $cookies = array();
-	private $url_start, $url_captcha, $post_url, $curl_info, $model, $headers;
+	private $url_start, $url_captcha, $post_url, $curl_info, $model, $headers, $follow_location = true;
 
 	function __construct() {
 
@@ -13,13 +13,10 @@ class Scrap {
 		$this->cookies[] = $cookie;
 	}
 
-	public function addDinamicFields($fields_dinamic) {
-		$this->fields_dinamic[] = $fields_dinamic;
-	}
-
 	public function setURLInicial($url_start) {
 		$this->url_start = $url_start;
 	}
+	
 	public function setCaptchaURL($url_captcha) {
 		$this->url_captcha = $url_captcha;
 	}
@@ -44,12 +41,23 @@ class Scrap {
 		$this->model = $model;
 	}
 
+	public function setFollowLocation($follow_location) {
+		$this->follow_location = $follow_location;
+	}
+
+	/**
+	 * Send HTTP request
+	 * @param string $url
+	 * @param string $method (default: GET)
+	 * @param string $params
+	 * @return string
+	 */
 	public function sendRequest($url, $method = 'GET', $params = '') {
 		$chOcr = curl_init();
 
 		curl_setopt($chOcr, CURLOPT_URL, $url);		
 		curl_setopt($chOcr, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($chOcr, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($chOcr, CURLOPT_FOLLOWLOCATION, $this->follow_location);
 		curl_setopt($chOcr, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($chOcr, CURLOPT_SSL_VERIFYHOST, 2);
 		curl_setopt($chOcr, CURLOPT_SSLVERSION, $this->SSL_VERSION);
@@ -69,13 +77,13 @@ class Scrap {
 		if ($method == 'POST') {			
 			curl_setopt($chOcr, CURLOPT_POST, 1);
 			curl_setopt($chOcr, CURLOPT_POSTFIELDS, $params);
-		}		
+		}
 		
 		$result = curl_exec ($chOcr);
 		$this->curl_info = curl_getinfo($chOcr);
 		curl_close ($chOcr);
 
-		return $result;
+		return (string)$result;
 	}
 
 	public function getInfo() {
